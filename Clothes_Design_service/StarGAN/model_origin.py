205adc4c2d919d7a24b560b41be804e701039fca
BitnaKeum <beausty23@naver.com>
2021-05-20 14:49:40 +0900

    최종 버전

--- Clothes_Design_service/StarGAN/model_origin.py
+++ Clothes_Design_service/StarGAN/model_origin.py
@@ -64,8 +64,8 @@ class Generator(nn.Module):
         # This is because instance normalization ignores the shifting (or bias) effect.
         c = c.view(c.size(0), c.size(1), 1, 1)
         c = c.repeat(1, 1, x.size(2), x.size(3))
-        c = c.to(self.device)  # ì¶”ê°€
-        x = x.to(self.device)  # ì¶”ê°€
+        c = c.to(self.device)
+        x = x.to(self.device)
         x = torch.cat([x, c], dim=1)  # concatenate
         return self.main(x)
 
